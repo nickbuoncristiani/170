bf823c4bb3ab00eb484bf7bfabb5dfd75b6c6d7f
nickbuoncristiani <nick.buoncristiani@berkeley.edu>
2019-11-18 13:09:13 -0800

    asdf

--- STSP.py
+++ STSP.py
@@ -1,14 +1,29 @@
-"""
-Space for figuring out how to run STSP on networkx 
-"""
+
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.algorithms import approximation
-import mip 
+    
 
+"""
+Gets graph which can be interpreted by TSP to give STSP through vertices
+"""    
+def get_complete_graph(G, vertices):
+    l=nx.all_pairs_shortest_path_length(G)
+    C=nx.Graph(vertices)
+    for vertex in l:
+        if vertex[0] in vertices:
+            for v in vertex[1]:
+                if v in vertices:
+                    C.add_edge(vertex[0], v, weight=vertex[1][v])
+    return C
 
+def 
+    
 if __name__ == "__main__":
     G=nx.Graph([(1, 2), (2, 3), (3, 4), (4, 1)])
-    S=approximation.steiner_tree(G, [1, 2, 3])
-    nx.draw_networkx(S, with_labels=True)
-    plt.show()
+    l=nx.all_pairs_shortest_path_length(G)
+    
+    #S=approximation.steiner_tree(G, [1, 2])
+    #nx.draw_networkx(S, with_labels=True)
+    #plt.show()
+
