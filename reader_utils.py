aca600cd0cb6b8a83c1da0b027a354eb6f2a6b9f
Giovanni Lu <glb@berkeley.edu>
2019-11-21 11:09:22 -0800

    fixed cycle->dropoff_nodes issue; added additional list structure closed_path to compensate

--- reader_utils.py
+++ reader_utils.py
@@ -67,10 +67,12 @@ def write_output(dropoffs, keys, output):
     with open(output, 'w+') as file:
         keys = {v: k for k, v in keys.items()}
         dropoff_nodes = []
+        closed_path = []
         len_dropoffs = len(dropoffs)
         for i in range(len_dropoffs - 1):
             stop_i = keys[dropoffs[i][0]]
             file.write(stop_i + ' ')
+            closed_path.append(stop_i)
             if len(dropoffs[i][1]) != 0:
                 dropoff_nodes.append(stop_i)
         file.write(keys[dropoffs[len_dropoffs - 1][0]] + '\n')
@@ -80,8 +82,8 @@ def write_output(dropoffs, keys, output):
             len_drop_i = len(dropoffs[i][1])
             if len_drop_i != 0:
                 #if i == 0:
-                    #file.write(dropoff_nodes[i] + '\n')
-                file.write(dropoff_nodes[i] + ' ')
+                    #file.write(closed_path[i] + '\n')
+                file.write(closed_path[i] + ' ')
                 len_drop_list_i = list(dropoffs[i][1])
                 for j in range(len_drop_i - 1):
                     file.write(keys[len_drop_list_i[j]] + ' ')
@@ -91,7 +93,7 @@ def write_output(dropoffs, keys, output):
 if __name__ == "__main__":
     inputs = read_input('spec.in')
     g = inputs[0]
-    print(STSP.ta_dropoff(g, [inputs[2]] + inputs[1]))
-    nx.draw_networkx(g, show_labels=True)
-    plt.show()    
+    #print(STSP.ta_dropoff(g, [specs[2]] + specs[1]))
+    #nx.draw_networkx(g, show_labels=True)
+    #plt.show()    
     write_output(STSP.ta_dropoff(g, [inputs[2]] + inputs[1]), inputs[3], 'output.out')
